ec7a56f99da16f79515b17a3c8e9aadd95a96a4a
samir25648 <samir.alanya@tecsup.edu.pe>
2023-05-08 00:06:21 -0500

    Agregar archivos de proyecto.

---
+++ Lab06/Lab06/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06
+{
+    public class Group : List<Item>
+    {
+        public string Title { get; set; }
+
+        public Group(string title)
+        {
+            Title = title;
+        }
+    }
+}
---
+++ Lab06/Lab06/ListViewGroup.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            Groups = new ObservableCollection<Group>
+            {
+            new Group("A")
+            {
+                new Item { Title = "Alanya", Subtitle = "Samir" },
+                new Item { Title = "Apaza", Subtitle = "Mateo" },
+                new Item { Title = "Aguilar", Subtitle = "Valentina" },
+                new Item { Title = "Arévalo", Subtitle = "Juan" }
+            },
+            new Group("B")
+            {
+                new Item { Title = "Bravo", Subtitle = "Lucas" },
+                new Item { Title = "Barrios", Subtitle = "Emma" },
+                new Item { Title = "Beltrán", Subtitle = "Alejandro" },
+                new Item { Title = "Bautista", Subtitle = "Sebastián" }
+            },
+            new Group("C")
+            {
+                new Item { Title = "Castro", Subtitle = "Liam" },
+                new Item { Title = "Cortés", Subtitle = "Olivia" },
+                new Item { Title = "Cervantes", Subtitle = "Gabriel" },
+                new Item { Title = "Campos", Subtitle = "Isabella" }
+            },
+            };
+            GroupedView.ItemsSource = Groups;
+        }
+    }
+}
